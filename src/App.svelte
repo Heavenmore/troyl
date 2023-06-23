637e4fc61c48bb8aa274a9e41b502738ca6a6497
troylsite <136275590+troylsite@users.noreply.github.com>
2023-06-23 19:48:33 +0000

    gist link

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,10 @@
   let markdown: string;
   let errMsg: string;
 
-  fetch("./content.md?t=" + Date.now())
+  fetch(
+    "https://gist.githubusercontent.com/troylsite/2a2c912a9d9b4221dae69b7129dadad1/raw/4526a25b3fabf4e2006e1e4c3704dbb6b28f7077/troyl.md?t=" +
+      Date.now()
+  )
     .then((res) => {
       if (res.status !== 200)
         throw new Error("" + res.status + " " + res.statusText);
